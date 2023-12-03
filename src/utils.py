26e8d77c4b867b4096c1a9241b66e7a94c3dfb1b
haleyej <haleyej@umich.edu>
2023-12-03 12:43:56 -0500

    added doc string + extra args to utils func

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,22 @@ import pandas as pd
 from pandas import DataFrame 
 
 
-def convert_dummies(cols: list, df: DataFrame) -> DataFrame:
+def convert_dummies(cols: list, df: DataFrame, drop:bool = False) -> DataFrame:
+    '''
+    helper function to add dummy variables 
+
+    ARGS:
+        cols: list of columns to make dummies for 
+        df: dataframe that contains 'cols'
+        drop: boolean value for wether or not to drop the original columns
+    
+    RETURNS:
+        new dataframe with dummy variables
+    '''
     for col in cols: 
         dummies = pd.get_dummies(df[col], drop_first = True)
         df = pd.concat([df, dummies], axis = 1)
     
-    df = df.drop(columns = cols)
+    if drop:
+        df = df.drop(columns = cols)
     return df
